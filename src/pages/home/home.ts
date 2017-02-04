cd053aa4410c5383075e09de3c071cce0adefdff
Mike Ulvila <mike.ulvila@premisehealth.com>
2017-02-03 18:43:33 -0600

    refactor page transition using [navPush] directive

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,20 +1,11 @@
 import { Component } from '@angular/core';
-
-import { NavController } from 'ionic-angular';
 import { UsersPage } from '../users/users'
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
-  }
-
-  onGoToUsers() {
-    this.navCtrl.push(UsersPage);
-  }
-
+  usersPage = UsersPage;
 }
